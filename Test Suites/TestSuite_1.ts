0dd7a2919bb8e6b5274b6d9684f29169e0098301
mirela.carla.bianor <mirela.carla.bianor@WINAPiL7g1DVdlF.corp.avanade.org>
2021-05-24 17:28:51 -0300

    Test katalon git

---
+++ Test Suites/TestSuite_1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite_1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>aaaba4e5-d5f0-4481-9ecd-a51c77bf1b78</testSuiteGuid>
+   <testCaseLink>
+      <guid>263eeddc-f232-42d3-b135-62e2b172c372</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase_1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
